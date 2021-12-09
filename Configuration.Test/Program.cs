d8895426720ccf1877fef994837efc6c07379f19
inbruk <35333872+inbruk@users.noreply.github.com>
2021-12-10 01:04:53 +0300

    Add project files.

---
+++ Configuration.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Configuration.Auxiliary.Helpers;
+
+namespace Configuration.Auxiliary.Helpers
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string sectionName = "Alpha";
+            string itemName = "Betta";
+            string Gamma = Configuration.GetItem(sectionName, itemName);
+            Console.WriteLine("Alpha:Betta => {0}", Gamma);
+
+            sectionName = "Delta";
+            itemName = "Epsilon";
+            string Dzeta = Configuration.GetItem(sectionName, itemName);
+            Console.WriteLine("Delta:Epsilon => {0}", Dzeta);
+
+            Console.ReadKey();
+        }
+    }
+}
